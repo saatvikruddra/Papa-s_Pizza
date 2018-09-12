11fee1a29a5334274435f5deea3efbfd3cf2a8cd
saatvikruddra <saatrud95@gmail.com>
2018-09-12 13:10:05 +0530

    New pop-up and plus minus added

--- Papa's Pizza.js
+++ Papa's Pizza.js
@@ -1,39 +1,41 @@
-		 		var modal = document.getElementById("pop-up")					
+		 		var modal = document.getElementById("pop-up")	
+		 		var modal2 = document.getElementById("pop-up1")				
+
 				var pizza = document.getElementById("pizza")
 				var burger = document.getElementById("burger")
 				var drinks = document.getElementById("drinks")				
 				
   				var span = document.getElementById("close")
-
-				pizza.onclick = function() {modal.style.display = 'block';
-														//document.getElementById("pop-up-content").innerHTML = "Thank-You For Choosing Our Amazing Pizza"
-														}										
-				burger.onclick = function() {modal.style.display = 'block';
-												   //document.getElementById("pop-up-content").innerHTML = "Thank-You For Choosing Our Awesome Burger"
-														}	
-				drinks.onclick = function() {modal.style.display = 'block';
-													//	document.getElementById("pop-up-content").innerHTML = "Thank-You For Choosing Our Ultimate Coffees"
-														}									
+  				
+  				
+				 pizza.onclick = function() {modal.style.display = 'block'}	
+				burger.onclick = function() {modal.style.display = 'block'}	
+				drinks.onclick = function() {modal.style.display = 'block'}									
 			
 				
-				span.onclick = function() {
-					modal.style.display = 'none';
-						}										 					
+				span.onclick = function() {modal.style.display = 'none'
+													modal2.style.display = 'none'}
+														 					
 		 		window.onclick = function(event) {
-	    		if (event.target == modal) {
+	    		if (event.target == modal || event.target == modal2) {
 	    			modal.style.display = 'none';
+	    			modal2.style.display = 'none';
 	    			}
 	    		} 
 	    		
-	    		
-	   //		var plus= document.getElementById("quantity1");
-	    	//	var minus=document.getElementById("quantity2");
-				//var amount = document.getElementById("quantNo");	    		
-	    		
-	 function plus(){
-	   		getElementById("qauntNo").innerHTML = getElementById("quantNo").value + 1;
-	    		}
-	    		
-	 function minus() { 
-	    		document.getElementById("qauntNo").innerHTML = document.getElementById("quantNo").value - 1;
-	    		}
+var x="";
+	    	
+		function incrementValue(x,i)
+{
+    var value = parseInt(x.value, 10);
+    value++;
+    x.value = value;
+}
+
+
+		function decrementValue(x,i)
+{
+    var value = parseInt(x.value, 10);
+    value--;
+    x.value = value;
+}	    		
